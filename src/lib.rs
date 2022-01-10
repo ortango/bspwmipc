298cd247e9332c4ea2241e250ddeeeba13ef5616
ortango <8408464+ortango@users.noreply.github.com>
2022-01-10 16:17:40 -0500

    correct formatting via fmt

--- src/lib.rs
+++ src/lib.rs
@@ -4,103 +4,110 @@ use std::os::unix::net::UnixStream;
 use std::{env, fmt, io};
 extern crate serde;
 pub mod reply;
-use reply::{BspwmState,Monitor,Desktop,Node};
+use reply::{BspwmState, Desktop, Monitor, Node};
 
 const BUFSIZ: usize = 8192;
 const SOCKET_ENV_VAR: &str = "BSPWM_SOCKET";
 
 #[derive(Debug)]
 pub enum EstablishError {
-	GetSocketPathError(io::Error),
-	SocketError(io::Error),
+    GetSocketPathError(io::Error),
+    SocketError(io::Error),
 }
 
 impl Error for EstablishError {
-	fn description(&self) -> &str {
-		match *self {
-			EstablishError::GetSocketPathError(_) => "Couldn't determine bspwm's socket path",
-			EstablishError::SocketError(_) => "Found bspwm's socket path but failed to connect",
-		}
-	}
-	fn cause(&self) -> Option<&dyn Error> {
-		match *self {
-			EstablishError::GetSocketPathError(ref e) | EstablishError::SocketError(ref e) => {
-				Some(e)
-			}
-		}
-	}
+    fn description(&self) -> &str {
+        match *self {
+            EstablishError::GetSocketPathError(_) => "Couldn't determine bspwm's socket path",
+            EstablishError::SocketError(_) => "Found bspwm's socket path but failed to connect",
+        }
+    }
+    fn cause(&self) -> Option<&dyn Error> {
+        match *self {
+            EstablishError::GetSocketPathError(ref e) | EstablishError::SocketError(ref e) => {
+                Some(e)
+            }
+        }
+    }
 }
 
 impl fmt::Display for EstablishError {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		write!(f, "{}", self)
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self)
+    }
 }
 
 //TODO: correctly set fallback socket value
 fn get_socket_path() -> io::Result<String> {
-	    if let Ok(sockpath) = env::var(SOCKET_ENV_VAR) {
-		            return Ok(sockpath);
-	    }
-	    return Ok(format!("/tmp/bspwm{}_{}_{}-socket", "", 0, 0));
+    if let Ok(sockpath) = env::var(SOCKET_ENV_VAR) {
+        return Ok(sockpath);
+    }
+    return Ok(format!("/tmp/bspwm{}_{}_{}-socket", "", 0, 0));
 }
 
 trait BspwmFuncs {
-	fn send_receive_bspwm_message(&mut self, message: &str) -> String;
+    fn send_receive_bspwm_message(&mut self, message: &str) -> String;
 }
 
 impl BspwmFuncs for UnixStream {
-	fn send_receive_bspwm_message(&mut self, message: &str) -> String {
-		let mut i = 0;
-		let mut buffer = [0; BUFSIZ];
-		for s in message.split_whitespace() {
-			for c in s.chars() {
-				buffer[i] = c as u8;
-				i += 1;
-			}
-			buffer[i] = 0;
-			i += 1;
-		}
-		self.write_all(&buffer[..i]).unwrap();
-		let mut response = String::new();
-		self.read_to_string(&mut response).expect("failed to communicate with bspwm.");
-		response
-	}
+    fn send_receive_bspwm_message(&mut self, message: &str) -> String {
+        let mut i = 0;
+        let mut buffer = [0; BUFSIZ];
+        for s in message.split_whitespace() {
+            for c in s.chars() {
+                buffer[i] = c as u8;
+                i += 1;
+            }
+            buffer[i] = 0;
+            i += 1;
+        }
+        self.write_all(&buffer[..i]).unwrap();
+        let mut response = String::new();
+        self.read_to_string(&mut response)
+            .expect("failed to communicate with bspwm.");
+        response
+    }
 }
 
 #[derive(Debug)]
 pub struct BspwmConnection {
-	    stream: UnixStream,
+    stream: UnixStream,
 }
 
 impl BspwmConnection {
-	pub fn connect() -> Result<BspwmConnection, EstablishError> {
-		match get_socket_path() {
-			Ok(path) => match UnixStream::connect(path) {
-				Ok(stream) => Ok(BspwmConnection { stream }),
-				Err(error) => Err(EstablishError::SocketError(error)),
-			},
-				Err(error) => Err(EstablishError::GetSocketPathError(error)),
-			}
-	}
-	// TODO: add support of status (all) and possible string return.
-	pub fn raw_command(&mut self, message: &str) {
-		let _reply = self.stream.send_receive_bspwm_message(message);
-	}
-	pub fn get_bspwm_state(&mut self) -> BspwmState {
-		let reply = self.stream.send_receive_bspwm_message("wm -d");
-		serde_json::from_str(&reply).unwrap()
-	}
-	pub fn get_monitor(&mut self, id: &u32) -> Monitor {
-		let reply = self.stream.send_receive_bspwm_message(&("query -T -m ".to_owned() + &id.to_string()));
-		serde_json::from_str(&reply).unwrap()
-	}
-	pub fn get_desktop(&mut self, id: &u32) -> Desktop {
-		let reply = self.stream.send_receive_bspwm_message(&("query -T -d ".to_owned() + &id.to_string()));
-		serde_json::from_str(&reply).unwrap()
-	}
-	pub fn get_node(&mut self, id: &u32) -> Node {
-		let reply = self.stream.send_receive_bspwm_message(&("query -T -n ".to_owned() + &id.to_string()));
-		serde_json::from_str(&reply).unwrap()
-	}
+    pub fn connect() -> Result<BspwmConnection, EstablishError> {
+        match get_socket_path() {
+            Ok(path) => match UnixStream::connect(path) {
+                Ok(stream) => Ok(BspwmConnection { stream }),
+                Err(error) => Err(EstablishError::SocketError(error)),
+            },
+            Err(error) => Err(EstablishError::GetSocketPathError(error)),
+        }
+    }
+    // TODO: add support of status (all) and possible string return.
+    pub fn raw_command(&mut self, message: &str) {
+        let _reply = self.stream.send_receive_bspwm_message(message);
+    }
+    pub fn get_bspwm_state(&mut self) -> BspwmState {
+        let reply = self.stream.send_receive_bspwm_message("wm -d");
+        serde_json::from_str(&reply).unwrap()
+    }
+    pub fn get_monitor(&mut self, id: &u32) -> Monitor {
+        let reply = self
+            .stream
+            .send_receive_bspwm_message(&("query -T -m ".to_owned() + &id.to_string()));
+        serde_json::from_str(&reply).unwrap()
+    }
+    pub fn get_desktop(&mut self, id: &u32) -> Desktop {
+        let reply = self
+            .stream
+            .send_receive_bspwm_message(&("query -T -d ".to_owned() + &id.to_string()));
+        serde_json::from_str(&reply).unwrap()
+    }
+    pub fn get_node(&mut self, id: &u32) -> Node {
+        let reply = self
+            .stream
+            .send_receive_bspwm_message(&("query -T -n ".to_owned() + &id.to_string()));
+        serde_json::from_str(&reply).unwrap()
+    }
 }
--- src/reply.rs
+++ src/reply.rs
@@ -3,185 +3,185 @@ use serde::Deserialize;
 #[derive(Deserialize, Debug)]
 #[serde(rename_all(deserialize = "camelCase"))]
 pub struct BspwmState {
-	pub clients_count: u32,
-	pub focus_history: Vec<Coordinates>,
-	pub focused_monitor_id: u32,
-	pub monitors: Vec<Monitor>,
-	pub primary_monitor_id: u32,
-	pub stacking_list: Vec<u32>,
+    pub clients_count: u32,
+    pub focus_history: Vec<Coordinates>,
+    pub focused_monitor_id: u32,
+    pub monitors: Vec<Monitor>,
+    pub primary_monitor_id: u32,
+    pub stacking_list: Vec<u32>,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all(deserialize = "camelCase"))]
 pub struct Monitor {
-	pub border_width: u16,
-	pub desktops: Vec<Desktop>,
-	pub focused_desktop_id: u32,
-	pub id: u32,
-	pub name: String,
-	pub padding: Padding,
-	pub randr_id: u32,
-	pub rectangle: Rectangle,
-	pub sticky_count: u32,
-	pub window_gap: i32,
-	pub wired: bool
+    pub border_width: u16,
+    pub desktops: Vec<Desktop>,
+    pub focused_desktop_id: u32,
+    pub id: u32,
+    pub name: String,
+    pub padding: Padding,
+    pub randr_id: u32,
+    pub rectangle: Rectangle,
+    pub sticky_count: u32,
+    pub window_gap: i32,
+    pub wired: bool,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all(deserialize = "camelCase"))]
 pub struct Desktop {
-	pub border_width: u16,
-	pub focused_node_id: u32,
-	pub id: u32,
-	pub layout: Layout,
-	pub name: String,
-	pub padding: Padding,
-	pub root: Option<Node>,
-	pub user_layout: Layout,
-	pub window_gap: i32
+    pub border_width: u16,
+    pub focused_node_id: u32,
+    pub id: u32,
+    pub layout: Layout,
+    pub name: String,
+    pub padding: Padding,
+    pub root: Option<Node>,
+    pub user_layout: Layout,
+    pub window_gap: i32,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all(deserialize = "camelCase"))]
 pub struct Node {
-	pub client: Option<Client>,
-	pub constraints: Constraints,
-	pub first_child: Option<Box<Node>>,
-	pub hidden: bool,
-	pub id: u32,
-	pub locked: bool,
-	pub marked: bool,
-	pub presel: Option<Presel>,
-	pub private: bool,
-	pub rectangle: Rectangle,
-	pub second_child: Option<Box<Node>>,
-	pub split_ratio: f64,
-	pub split_type: SplitType,
-	pub sticky: bool,
-	pub vacant: bool
+    pub client: Option<Client>,
+    pub constraints: Constraints,
+    pub first_child: Option<Box<Node>>,
+    pub hidden: bool,
+    pub id: u32,
+    pub locked: bool,
+    pub marked: bool,
+    pub presel: Option<Presel>,
+    pub private: bool,
+    pub rectangle: Rectangle,
+    pub second_child: Option<Box<Node>>,
+    pub split_ratio: f64,
+    pub split_type: SplitType,
+    pub sticky: bool,
+    pub vacant: bool,
 }
 
 impl Node {
-	pub fn traverse(&self) -> Vec<&Node> {
-		let mut stack: Vec<&Node> = Vec::new();
-		let mut res: Vec<&Node> = Vec::new();
-		stack.push(self);
-		while !stack.is_empty() {
-			let node = stack.pop().unwrap();
-			res.push(node);
-			match node.first_child {
-				None => {}
-				Some(ref n) => stack.push(n),
-			}
-			match node.second_child {
-				None => {}
-				Some(ref n) => stack.push(n),
-			}
-		}
-		res
-	}
+    pub fn traverse(&self) -> Vec<&Node> {
+        let mut stack: Vec<&Node> = Vec::new();
+        let mut res: Vec<&Node> = Vec::new();
+        stack.push(self);
+        while !stack.is_empty() {
+            let node = stack.pop().unwrap();
+            res.push(node);
+            match node.first_child {
+                None => {}
+                Some(ref n) => stack.push(n),
+            }
+            match node.second_child {
+                None => {}
+                Some(ref n) => stack.push(n),
+            }
+        }
+        res
+    }
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all(deserialize = "camelCase"))]
 pub struct Client {
-	pub border_width: u16,
-	pub class_name: String,
-	pub floating_rectangle: Rectangle,
-	pub instance_name: String,
-	pub last_layer: Layer,
-	pub last_state: State,
-	pub layer: Layer,
-	pub shown: bool,
-	pub state: State,
-	pub tiled_rectangle: Rectangle,
-	pub urgent: bool
+    pub border_width: u16,
+    pub class_name: String,
+    pub floating_rectangle: Rectangle,
+    pub instance_name: String,
+    pub last_layer: Layer,
+    pub last_state: State,
+    pub layer: Layer,
+    pub shown: bool,
+    pub state: State,
+    pub tiled_rectangle: Rectangle,
+    pub urgent: bool,
 }
 
 impl Client {
-	pub fn get_geometry(&self) -> &Rectangle {
-		match self.state {
-			State::Floating => &self.floating_rectangle,
-			_ => &self.tiled_rectangle,
-		}
-	}
+    pub fn get_geometry(&self) -> &Rectangle {
+        match self.state {
+            State::Floating => &self.floating_rectangle,
+            _ => &self.tiled_rectangle,
+        }
+    }
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all(deserialize = "camelCase"))]
 pub struct Presel {
-	pub split_dir: Direction,
-	pub split_ratio: f64
+    pub split_dir: Direction,
+    pub split_ratio: f64,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all(deserialize = "camelCase"))]
 pub struct Padding {
-	pub top: i16,
-	pub right: i16,
-	pub bottom: i16,
-	pub left: i16
+    pub top: i16,
+    pub right: i16,
+    pub bottom: i16,
+    pub left: i16,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all(deserialize = "camelCase"))]
 pub struct Rectangle {
-	pub x: i16,
-	pub y: i16,
-	pub width: u16,
-	pub height: u16
+    pub x: i16,
+    pub y: i16,
+    pub width: u16,
+    pub height: u16,
 }
 
 #[derive(Deserialize, Debug)]
 pub struct Constraints {
-	pub min_width: u16,
-	pub min_height: u16
+    pub min_width: u16,
+    pub min_height: u16,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all(deserialize = "camelCase"))]
 pub struct Coordinates {
-	pub monitor_id: u32,
-	pub desktop_id: u32,
-	pub node_id: u32
+    pub monitor_id: u32,
+    pub desktop_id: u32,
+    pub node_id: u32,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all(deserialize = "snake_case"))]
 pub enum Layout {
-	Tiled,
-	Monocle
+    Tiled,
+    Monocle,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all(deserialize = "snake_case"))]
 pub enum SplitType {
-	Horizontal,
-	Vertical
+    Horizontal,
+    Vertical,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all(deserialize = "snake_case"))]
 pub enum Direction {
-	North,
-	West,
-	South,
-	East
+    North,
+    West,
+    South,
+    East,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all(deserialize = "snake_case"))]
 pub enum Layer {
-	Below,
-	Normal,
-	Above
+    Below,
+    Normal,
+    Above,
 }
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all(deserialize = "snake_case"))]
 pub enum State {
-	Tiled,
-	PseudoTiled,
-	Floating,
-	Fullscreen
+    Tiled,
+    PseudoTiled,
+    Floating,
+    Fullscreen,
 }
